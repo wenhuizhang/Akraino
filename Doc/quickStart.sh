ef2ebdaaf4dc3afe23d57b17cbff2756658b21cf
Wenhui Zhang <wenhui@gwmail.gwu.edu>
2018-06-29 16:16:35 -0400

    Create quickStart.sh

---
+++ Doc/quickStart.sh
@@ -0,0 +1,9 @@
+# Quick Start
+
+There are three main components in this project: 
+
+1.  Global control node (Slave Jankins)
+
+2.  Regional node (where OS, protal, Camunda workflow, Postgresdb and LDAP)
+
+3.  Edge node (which is the one depolyed through airship scripts)
