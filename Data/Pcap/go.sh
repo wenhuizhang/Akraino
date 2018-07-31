054fd1c9488ba50a5e94c9df11562018eff8263a
wenhuizhang <wenhui@gwmail.gwu.edu>
2018-07-31 14:57:20 +0000

    add 10M scripts

---
+++ Data/Pcap/go.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+rm /var/log/suricata/eve.json  /var/log/suricata/fast.log  /var/log/suricata/stats.log  /var/log/suricata/suricata.log
+./run0.sh
+
+rm /var/log/suricata/eve.json  /var/log/suricata/fast.log  /var/log/suricata/stats.log  /var/log/suricata/suricata.log
+./run1.sh
+
+rm /var/log/suricata/eve.json  /var/log/suricata/fast.log  /var/log/suricata/stats.log  /var/log/suricata/suricata.log
+./run2.sh
+
+rm /var/log/suricata/eve.json  /var/log/suricata/fast.log  /var/log/suricata/stats.log  /var/log/suricata/suricata.log
+./run3.sh
+
+rm /var/log/suricata/eve.json  /var/log/suricata/fast.log  /var/log/suricata/stats.log  /var/log/suricata/suricata.log
+./run4.sh
+
+rm /var/log/suricata/eve.json  /var/log/suricata/fast.log  /var/log/suricata/stats.log  /var/log/suricata/suricata.log
+./run5.sh
+
+rm /var/log/suricata/eve.json  /var/log/suricata/fast.log  /var/log/suricata/stats.log  /var/log/suricata/suricata.log
---
+++ Data/Pcap/stat.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "..........................................."
+sort -n result.txt | uniq -c
+
+echo "..........................................."
+sort -n result0.txt | uniq -c
+
+echo "..........................................."
+sort -n result2.txt | uniq -c
+
+echo "..........................................."
+sort -n result3.txt | uniq -c
+
+echo "..........................................."
+sort -n result4.txt | uniq -c
+
+echo "..........................................."
+sort -n result5.txt | uniq -c
